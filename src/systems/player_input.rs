75f5d612df89be8a3c2c8eac126122ecfe8f486d
Oliver Säfström <safstrom.oliver@gmail.com>
2023-03-06 09:38:00 +0100

    Add entity_render

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -12,8 +12,8 @@ pub fn player_input(ecs: &mut SubWorld,
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
             VirtualKeyCode::Right => Point::new(1, 0),
-            VirtualKeyCode::Down => Point::new(0, -1),
-            VirtualKeyCode::Up => Point::new(0, 1),
+            VirtualKeyCode::Down => Point::new(0, 1),
+            VirtualKeyCode::Up => Point::new(0, -1),
             _ => Point::new(0, 0),
         };
         if delta.x != 0 || delta.y != 0 {
