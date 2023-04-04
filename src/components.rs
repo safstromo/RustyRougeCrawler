9e5dbfa5edc927f795bcb16c849ce80291d77c03
Oliver Säfström <safstrom.oliver@gmail.com>
2023-04-04 10:16:10 +0200

    Add WantsToAttack component

--- src/components.rs
+++ src/components.rs
@@ -20,10 +20,18 @@ pub struct WantsToMove {
     pub entity: Entity,
     pub destination: Point,
 }
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Health {
     pub current: i32,
     pub max: i32,
 }
+
 #[derive(Clone, PartialEq)]
-pub struct Name(pub String); 
+pub struct Name(pub String);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct WantsToAttack {
+    pub attacker: Entity,
+    pub victim: Entity,
+}
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,7 +1,6 @@
 mod player_input;
 mod map_render;
 mod entity_render;
-mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
@@ -25,8 +24,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
@@ -41,8 +38,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
+
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
